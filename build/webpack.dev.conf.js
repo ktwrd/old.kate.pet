66f23ad5d86cb1fd27689f1c130b91cce1d3eaa4
Kate Ward <kate@dariox.club>
2022-01-09 21:40:24 +0800

    Update webpack.dev.conf.js

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -10,8 +10,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
+process.env.PORT = 80
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
+
 
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
@@ -22,6 +24,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
   // these devServer options should be customized in /config/index.js
   devServer: {
+    disableHostCheck: true, 
     clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
