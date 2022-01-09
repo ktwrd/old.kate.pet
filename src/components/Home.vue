1e3c320f40e8a12aff3504125a12bedfdb1eb5c0
Kate Ward <kate@dariox.club>
2022-01-09 21:40:06 +0800

    Add Volume Slider

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,61 @@
 <template>
     <div>
-        <visualizer/>
+        <div class="backgroundElement" type="visualizer">
+            <visualizer ref="vis"/>
+        </div>
+        <div class="visualizerControls">
+            <input
+                type="range"
+                min="0"
+                max="1024"
+                step="0.1"
+                :value="volume"
+                @change="ChangeAudioValue"
+                ref="volume" />
+        </div>
     </div>
 </template>
+<style scoped>
+.backgroundElement {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: -100;
+    margin: 0;
+    padding: 0;
+    width: 100vw;
+    height: 100vh;
+}
+.backgroundElement[type=visualizer] canvas {
+    margin: 0;
+    padding: 0;
+    opacity: 0.2;
+    filter: blur(10px);
+    transition: 300ms;
+}
+</style>
 <script>
 import Visualizer from './Visualizer.vue';
 export default {
     name: 'Home',
-    components: {Visualizer}
+    components: {Visualizer},
+    data () {
+        return {
+            audioURL: 'http://res.kate.pet/pageaudio/bitmap_fantasy.mp3',
+            subtitle: '',
+            volume: 0.0
+        };
+    },
+    mounted () {
+        this.$refs.vis.audioURL = this.audioURL;
+        this.$refs.vis.loadAudioFromURL(this.audioURL);
+    },
+    methods: {
+        ChangeAudioValue () {
+            this.volume = this.$refs.volume.value;
+            this.$refs.vis.setVolume(this.$refs.volume.value / 1024);
+            console.log(this.$refs.vis.volumeNode.value, this.$refs.volume.value / 1024);
+        }
+    }
 };
 </script>
