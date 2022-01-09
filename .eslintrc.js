2a84e5fda74117b0a7d6cf0bcca8199f7a4280c0
Kate Ward <kate@dariox.club>
2022-01-09 23:57:18 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 
     'indent': ['error', 4],
-    'semi': ['error', 'always']
+    'semi': ['error', 'always'],
+    'no-unneeded-ternary': 0
   }
 }
