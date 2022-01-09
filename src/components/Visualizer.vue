3b59f3ad8b8a69ef6ef05274790d8d04a10ea71b
Kate Ward <kate@dariox.club>
2022-01-09 21:40:00 +0800

    Visualizer and Volume Change working

--- src/components/Visualizer.vue
+++ src/components/Visualizer.vue
@@ -1,28 +1,55 @@
 <template>
     <canvas
         ref='canvas'
-        :style="`width: ${width}; height: ${height};`"
+        v-bind:audioURL='audioURL'
+        v-bind:width='width'
+        v-bind:height='height'
     />
 </template>
+<style scoped>
+canvas {
+    margin: 0;
+    padding: 0;
+    display: block;
+}
+</style>
 <script>
 import butterchurn from 'butterchurn';
-import butterchurnPresets from 'butterchurn-presets';
+import ps1 from 'butterchurn-presets/lib/butterchurnPresets.min';
+import ps2 from 'butterchurn-presets/lib/butterchurnPresetsExtra.min';
+import ps3 from 'butterchurn-presets/lib/butterchurnPresetsExtra2.min';
+import ps4 from 'butterchurn-presets/lib/butterchurnPresetsMD1.min';
+import ps5 from 'butterchurn-presets/lib/butterchurnPresetsMinimal.min';
+import ps6 from 'butterchurn-presets/lib/butterchurnPresetsNonMinimal.min';
 
+var bcPresets = Object.assign({},
+    ps1.getPresets(),
+    ps2.getPresets(),
+    ps3.getPresets(),
+    ps4.getPresets(),
+    ps5.getPresets(),
+    ps6.getPresets());
+
+console.log(bcPresets);
 export default {
     name: 'Visualizer',
+    watch: {
+        audioURL (value) {
+            this.audioURL = value;
+        },
+        volume () {
+
+        }
+    },
     data () {
-        var presets = butterchurnPresets.getPresets();
+        var presets = bcPresets;
+        if (this.audioURL !== undefined && this.audioURL !== null) {
+            this.loadAudioFromURL(this.audioURL);
+        }
 
         return {
-            options: {
-                width: 800, // {int} - Render Dimension in Pixels
-                height: 600 // {int} - Render Dimension in Pixels
-            },
-
-            width: '100%',
-            height: '100%',
-
-            preset: 'Flexi, martin + geiss - dedicated to the sherwin maxawow',
+            // preset: 'flexi - what is the matrix',
+            preset: 'suksma - heretical crosscut playpen',
 
             // Number of seconds to blend presets
             presetBlend: 0.0,
@@ -32,74 +59,102 @@ export default {
             audioContext: null,
             source: null,
 
-            pixelRatio: window.devicePixelRatio || 1,
+            audioURL: null,
 
-            textureRatio: 1,
-
-            presets
+            presets: presets,
+            volume: 0.2,
+            playPosition: 0,
+            doRender: true,
+            renderInterval: 1000 / 120,
+            renderLoop: null,
+            height: window.innerHeight,
+            width: window.innerWidth,
+            volumeNode: null
         };
     },
     mounted () {
-        window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
-        if (this.audioContext == null) this.audioContext = new AudioContext();
-
-        if (this.visualizer == null) {
-            this.visualizer = butterchurn.createVisualizer(this.audioContext, this.$refs.canvas, {
-                width: this.options.width,
-                height: this.options.height,
-                pixelRatio: this.pixelRatio,
-                textureRatio: this.textureRatio
-            });
-        }
-        if (this.source == null) {
-            this.source = this.audioContext.createBufferSource();
-        }
-        this.visualizer.connectAudio(this.source);
-
-        this.visualizer.loadPreset(this.presets[this.preset], this.presetBlend);
-
-        this.visualizer.setRendererSize(this.$data.width, this.$data.height);
-
-        this.visualizer.render();
-        this.setCanvasSize();
-        this.loadAudioFromURL('https://cdn.jyles.club/pageaudio/chrome_vox.mp3');
+        let self = this;
+        // On window resize change the canvas size.
+        window.addEventListener('resize', () => {
+            self.$refs.canvas.width = window.innerWidth;
+            self.$refs.canvas.height = window.innerHeight;
+            this.$data.visualizer.setRendererSize(window.innerWidth, window.innerHeight);
+        });
     },
     methods: {
-        setCanvasSize (width = this.$data.width, height = this.$data.height) {
-            this.$data.width = width;
-            this.$data.height = height;
-        },
         setPreset (name, blend = this.$data.presetBlend) {
             this.$data.visualizer.loadPreset(this.$data.presets[name], blend);
         },
+        createVisualizer () {
+            window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+
+            this.$refs.canvas.width = window.innerWidth;
+            this.$refs.canvas.height = window.innerHeight;
+
+            var presets = bcPresets;
+            this.$data.visualizer = butterchurn.createVisualizer(this.$data.audioContext, this.$refs.canvas, {
+                width: 1600,
+                height: 900
+            });
+            console.log(this.$data.visualizer);
+            this.$data.visualizer.connectAudio(this.$data.source);
+
+            this.$data.visualizer.loadPreset(presets[this.preset], this.$data.presetBlend);
+
+            this.$data.source.start(this.$data.playPosition);
+            this.$data.renderLoop = setInterval(() => {
+                if (this.$data.doRender) this.$data.visualizer.render();
+            }, this.$data.renderInterval);
+            setTimeout(() => {
+                this.$data.visualizer.setRendererSize(window.innerWidth, window.innerHeight);
+            }, 100);
+        },
+        setVolume (value) {
+            this.$data.volumeNode.gain.setValueAtTime(value, this.$data.audioContext.currentTime);
+        },
         loadAudioFromURL (location) {
             try {
+                if (this.$data.audioContext == null) {
+                    this.$data.audioContext = new AudioContext();
+                }
+                if (this.$data.volumeNode == null) {
+                    this.$data.volumeNode = this.$data.audioContext.createGain();
+                    this.$data.volumeNode.value = this.$data.volume;
+                    this.$data.volumeNode.connect(this.$data.audioContext.destination);
+                }
+                if (this.$data.source == null) {
+                    this.$data.source = this.audioContext.createBufferSource();
+                } else {
+                    this.$data.source.stop();
+                }
+
                 // now retrieve some binary audio data from <audio>, ajax, input file or microphone and put it into a audio source object.
                 // here we will retrieve audio binary data via AJAX
                 var request = new XMLHttpRequest();
-                request.open('GET', location, true);
+                request.open('GET', location);
                 request.responseType = 'arraybuffer'; // This asks the browser to populate the retrieved binary data in a array buffer
+                let self = this;
                 request.onload = function () {
                     // populate audio source from the retrieved binary data. This can be done using decodeAudioData function.
                     // first parameter of decodeAudioData needs to be array buffer type. So from wherever you retrieve binary data make sure you get in form of array buffer type.
-                    this.audioContext.decodeAudioData(request.response, function (buffer) {
-                        this.source.buffer = buffer;
-                    }, null);
+                    self.$data.audioContext.decodeAudioData(request.response, function (buffer) {
+                        self.$data.source.buffer = buffer;
+                    });
                 };
                 request.send();
 
                 // now we got context, audio source.
                 // now lets connect the audio source to a destination(hardware to play sound).
-                this.source.connect(this.audioContext.destination); // destination property is reference the default audio device
+                this.$data.source.connect(this.$data.volumeNode); // destination property is reference the default audio device
 
                 /*
-                    If we wanted to add any audio nodes then we need to add them in between audio source and destionation anytime dynamically.
+                If we wanted to add any audio nodes then we need to add them in between audio source and destionation anytime dynamically.
                 */
-
-                // now play the sound.
-                this.source.start(0);
+                setTimeout(() => {
+                    this.createVisualizer();
+                }, 150);
             } catch (e) {
-                alert('Web Audio API not supported');
+                console.error(e);
             }
         }
     }
