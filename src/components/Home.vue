81b7d993c6d4edeed3b8ca52b3f9d6c8a11c8a98
Kate Ward <kate@dariox.club>
2022-01-09 23:57:10 +0800

    Finished Visualizer Controls (yay!)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,30 @@
 <template>
     <div>
-        <div class="backgroundElement" type="visualizer">
-            <visualizer ref="vis"/>
-        </div>
-        <div class="visualizerControls">
-            <input
-                type="range"
-                min="0"
-                max="1024"
-                step="0.1"
-                :value="volume"
-                @change="ChangeAudioValue"
-                ref="volume" />
-            <button
-                @click="ToggleAudio">
-                Play/Pause
-            </button>
-        </div>
+        <template v-if="enableVisualizer">
+            <div class="backgroundElement" type="visualizer">
+                <template v-if="visualizerComponent">
+                    <visualizer ref="vis" v-model="visualizer"/>
+                </template>
+            </div>
+            <div class="visualizerControls">
+                <input
+                    type="range"
+                    min="0"
+                    max="1024"
+                    step="0.1"
+                    :value="volume"
+                    @change="ChangeAudioValue"
+                    ref="volume" />
+                <input
+                    type="button"
+                    @click="ToggleAudio"
+                    v-bind:value="IsPlaying() ? 'Pause' : 'Play'"/>
+                <button
+                    @click="SelectNewRandomAudio()">
+                    New Song
+                </button>
+            </div>
+        </template>
     </div>
 </template>
 <style scoped>
@@ -40,28 +48,73 @@
 </style>
 <script>
 import Visualizer from './Visualizer.vue';
+
+const AudioSelect = require('./AudioSelect');
+
 export default {
     name: 'Home',
     components: {Visualizer},
     data () {
+        if (localStorage.AudioVolume === undefined) {
+            localStorage.AudioVolume = 0.3;
+        }
+        if (localStorage.AudioAutoplay === undefined) {
+            localStorage.AudioAutoplay = 'false';
+        }
+        if (localStorage.Visualizer === undefined) {
+            localStorage.Visualizer = 'true';
+        }
         return {
-            audioURL: 'http://res.kate.pet/pageaudio/bitmap_fantasy.mp3',
+            audioURL: null,
             audioObject: null,
             subtitle: '',
-            volume: 0.0
+            volume: parseFloat(localStorage.AudioVolume) * 1024,
+            playing: false,
+            autoplay: localStorage.AudioAutoplay === 'true' ? true : false,
+            visualizerComponent: true,
+            visualizer: null,
+            enableVisualizer: localStorage.Visualizer === 'true' ? true : false
         };
     },
     mounted () {
-        this.$refs.vis.audioURL = this.audioURL;
-        this.$refs.vis.loadAudioFromURL(this.audioURL);
+        if (this.$data.autoplay) {
+            this.ToggleAudio();
+        }
     },
     methods: {
         ChangeAudioValue () {
             this.volume = this.$refs.volume.value;
+            localStorage.AudioVolume = this.$refs.volume.value / 1024;
             this.$refs.vis.setVolume(this.$refs.volume.value / 1024);
-            console.log(this.$refs.vis.volumeNode.value, this.$refs.volume.value / 1024);
+            this.$refs.volume.value = this.$refs.volume.value / 1024;
         },
         ToggleAudio () {
+            if (this.$refs.vis.visualizer == null) {
+                this.SelectRandomAudio();
+            }
+            this.$refs.vis.playpause();
+            this.$data.playing = this.$refs.vis.playing;
+        },
+        SelectNewRandomAudio () {
+            this.$delete(this.$children, 0);
+            this.$refs.vis.kill();
+            this.SelectRandomAudio();
+            setTimeout(() => {
+                this.ToggleAudio();
+            }, 200);
+        },
+        IsPlaying () {
+            if (this.$refs.vis === undefined) return false;
+            return this.$refs.vis.$data.playing;
+        },
+        SelectRandomAudio () {
+            if (this.$refs.vis.playing) {
+                this.$refs.vis.playpause();
+            }
+            this.$data.audioObject = AudioSelect.select();
+            this.$data.audioURL = this.$data.audioObject.url;
+            this.$refs.vis.loadAudioFromURL(this.$data.audioURL);
+            this.ChangeAudioValue(this.$data.volume);
             this.$refs.vis.playpause();
         }
     }
