71194bb7dd2467a1465093d733903c648f7b2a73
Kate Ward <kate@dariox.club>
2022-01-29 00:41:47 +0800

    Custom Header icons!

---
+++ src/components/HeaderIcon.vue
@@ -0,0 +1,27 @@
+<template>
+    <img ref="image" :src="filename" @error="fallbackimage"/>
+</template>
+
+<style scoped>
+img {
+    height: 175px;
+}
+</style>
+
+<script>
+const items = require('./headericons');
+
+export default {
+    name: 'HeaderIcon',
+    data () {
+        return {
+            filename: `http://res.kate.pet/image/${items[Math.floor(Math.random() * items.length)]}`
+        };
+    },
+    methods: {
+        fallbackimage () {
+            this.$refs.image.attributes.src.value = 'http://res.kate.pet/image/petpet.gif';
+        }
+    }
+};
+</script>
