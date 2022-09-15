319de57fdc085e8f1bb3869126dce088d05c3d59
kate! <kate@dariox.club>
2022-09-15 21:28:02 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-    <div id="app">
-        <link rel="icon" href="/static/favicon.png" type="image/png" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <div style="position:fixed; top: 0;left:0;">
-            <b-button-group variant="outline-secondary">
-                <b-button variant="outline-secondary" style="border-top-left-radius: 0; border-bottom-left-radius: 0;" size="sm" to="/">Home</b-button>
-                <b-button variant="outline-secondary" style="border-radius:none" size="sm" to="/track-credits">Track Credits</b-button>
-                <b-button variant="outline-secondary" style="border-top-right-radius:0;" size="sm" to="/other">Other</b-button>
-            </b-button-group>
+    <div>
+        <div id="app">
+            <link rel="icon" href="/static/favicon.png" type="image/png" />
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <div style="position:fixed; top: 0;left:0;">
+                <b-button-group variant="outline-secondary">
+                    <b-button variant="outline-secondary" style="border-top-left-radius: 0; border-bottom-left-radius: 0;" size="sm" to="/">Home</b-button>
+                    <b-button variant="outline-secondary" style="border-radius:none" size="sm" to="/track-credits">Track Credits</b-button>
+                    <b-button variant="outline-secondary" style="border-top-right-radius:0;" size="sm" to="/other">Other</b-button>
+                </b-button-group>
+            </div>
+            <landing-page />
+            <div style="margin-top: 7rem;"><router-view/></div>
         </div>
-        <landing-page />
-        <div style="margin-top: 7rem;"><router-view/></div>
     </div>
 </template>
 <script>
@@ -32,6 +34,7 @@ body,
     background: rgba(0, 0, 0, 0) !important;
     background-color: rgba(0, 0, 0, 0) !important;
 }
+
 img {
     image-rendering: auto;
     image-rendering: crisp-edges;
