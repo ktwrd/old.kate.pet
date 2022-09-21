3e76ec4bcc8be1717ec503fe16c97fbfbf82347f
kate! <kate@dariox.club>
2022-09-21 22:22:14 +0800

    Rename /track-credits

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ export default new Router({
         },
         {
             path: '/track-credits',
-            name: 'Track Credits',
+            name: 'Playlist',
             component: TrackCredits
         }
     ]
