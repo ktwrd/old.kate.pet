c4a4eb5cede9cf3aef2d082797a77a0c8524e822
kate! <kate@dariox.club>
2022-09-21 22:22:23 +0800

    Add ability to play specific tracks

--- src/TrackCredits.vue
+++ src/TrackCredits.vue
@@ -1,13 +1,25 @@
 <template>
     <div class="container" style="height: calc(100vh - 10rem); overflow: auto;">
         <template v-for="(track, index) in availableObjects">
-            <div v-bind:key="`trackcredit-${index}`" class="track-container">
-                <template v-if="track.link.length > 8">
-                    <a href="#/track-credits" @click="(event) => trackActivate(event, track)"><div class="track-number">{{index+1}}.</div> {{track.name}}</a>
-                </template>
-                <template v-else>
-                    <div style="color: white;"><div class="track-number">{{index+1}}.</div> {{track.name}}</div>
-                </template>
+            <div v-bind:key="`trackcredit-${index}`" >
+                <b-row class="m-1">
+                    <b-col cols="auto">
+                        <b-button size="sm" @click="playTrack(track, index)"><b-icon icon="play-circle"></b-icon> </b-button>
+                    </b-col>
+                    <b-col cols="1">
+                        <div class="track-number">
+                            {{index+1}}.
+                        </div>
+                    </b-col>
+                    <b-col style="text-align: left;">
+                        <template v-if="track.link.length > 8">
+                            <a href="#/track-credits" @click="(event) => trackActivate(event, track)">{{track.name}}</a>
+                        </template>
+                        <template v-else>
+                            <div style="color: white;">{{track.name}}</div>
+                        </template>
+                    </b-col>
+                </b-row>
             </div>
         </template>
     </div>
@@ -18,6 +30,7 @@
     display: inline-block;
     font-family: monospace;
     text-align: right;
+    color: white;
 }
 .track-container {
     width: fit-content;
@@ -37,6 +50,17 @@ export default {
             if (track.link.toString().length > 8) {
                 window.open(track.link, '_blank').focus()
             }
+        },
+        async playTrack (track, index) {
+            console.log(this)
+            this.$root.$children[0].$refs.landingPage.$refs.home.ClearSelection();
+            this.$root.$children[0].$refs.landingPage.$refs.home.$delete(this.$root.$children[0].$refs.landingPage.$refs.home.$children, 0);
+            this.$root.$children[0].$refs.landingPage.$refs.home.$refs.vis.kill();
+            await this.$root.$children[0].$refs.landingPage.$refs.home.SetAudio({
+                ...track,
+                url: `https://res.kate.pet/pageaudio/${track.file}`
+            })
+            this.$root.$children[0].$refs.landingPage.$refs.home.ToggleAudio();
         }
     }
 }
